a17ce8fcec686f467f65b64e7c5e047ed244c0e6
elenabro <41024225+elenabro@users.noreply.github.com>
2020-07-07 10:51:15 -0700

    asutest

--- src/test/java/support/Hooks.java
+++ src/test/java/support/Hooks.java
@@ -1,6 +1,10 @@
 package support;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.Before;
+import org.junit.After;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,14 +23,14 @@ public class Hooks {
     }
 
 
-//    @After
-//    public void scenarioEnd(Scenario scenario) {
-//        if (scenario.isFailed()) {
-//            TakesScreenshot screenshotTaker = (TakesScreenshot) getDriver();
-//            byte[] screenshot = screenshotTaker.getScreenshotAs(OutputType.BYTES);
-//            scenario.embed(screenshot, "image/png");
-//        }
-//        TestContext.teardown();
-//    }
+    @After
+    public void scenarioEnd(Scenario scenario) {
+        if (scenario.isFailed()) {
+            TakesScreenshot screenshotTaker = (TakesScreenshot) getDriver();
+            byte[] screenshot = screenshotTaker.getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
+        TestContext.teardown();
+    }
 }
 
